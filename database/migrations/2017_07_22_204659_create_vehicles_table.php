792d6a0fe46bdfc1c074d413f6b2cdb93b90c248
Chandler Swift <chandler@chandlerswift.com>
2017-07-22 16:00:49 -0500

    Added tracks and vehicles

---
+++ database/migrations/2017_07_22_204659_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            // possible future expansion: $table->integer('owner_id')->nullable();
+            $table->text('description');
+            $table->text('earth_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
