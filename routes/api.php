40dab760089b463b12bb7df355a0ae82d830637d
Chandler Swift <chandler@chandlerswift.com>
2018-03-07 13:45:35 -0600

    Add API routes

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('/users', function() {
-    return \App\Users::all();
+Route::get('/users', function() {
+    return \App\User::select('id', 'name')->get();
+});
+
+Route::get('/tracks', function() {
+    return \App\Track::all();
+});
+
+Route::get('/vehicles', function() {
+    return \App\Vehicle::all();
 });
