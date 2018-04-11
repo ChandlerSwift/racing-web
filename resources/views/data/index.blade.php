55cfd97750b73acd3b58134081f4700f86dc900a
Chandler Swift <chandler@chandlerswift.com>
2018-04-11 15:18:20 -0500

    Add Export Data page

--- resources/views/data/index.blade.php
+++ resources/views/data/index.blade.php
@@ -4,7 +4,9 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">Export Data</h1>
-            TODO
+            <p>To download a complete dump of the data as a CSV file which can be opened with Excel, click the button below.</p>
+            <p>This includes all race data since 2015 by all racers, using all vehicles, across all tracks.</p>
+            <a class="btn btn-primary" href="/data/dump">Download Data</a>
         </div>
     </div>
 @endsection
